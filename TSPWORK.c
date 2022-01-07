2f26b856cab552b1eeace3d99519a5d408b9ac39
mishelld <93199677+mishelld@users.noreply.github.com>
2022-01-07 22:17:25 +0200

    Add files via upload

--- TSPWORK.c
+++ TSPWORK.c
@@ -5,20 +5,6 @@ int weight = infinity;
 int size;
 pnode graph;
 
-int *buildArr(int size)
-{
-
-    int *arr = (int *)malloc(sizeof(int) * size);
-    if (arr == NULL)
-    {
-        return NULL;
-    }
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    return arr;
-}
 
 int *copyArr(int *arr)
 {
@@ -119,7 +105,17 @@ int TSP_cmd(pnode head)
     {
         scanf("%d", &arr[i]);
     }
-    int *copy = copyArr(arr);
+    //int *copy = copyArr(arr);
+     int *copy = (int *)malloc(sizeof(int) * size);
+    if (copy == NULL)
+    {
+        copy =  NULL;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        copy[i] = arr[i];
+    }
+    
     permutations(0, copy);
     free(arr);
     free(copy);
